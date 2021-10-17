861180870caa79ec4ec647b9b49504c3bb0a6508
nonnax <inonnax@gmail.com>
2021-10-17 21:07:10 +0800

    code fix

--- ffstacks.rb
+++ ffstacks.rb
@@ -23,44 +23,37 @@ cuts = cuts_df.map # shared
 # 320 x 240 (or 240p)
 
 # scale=%w[320 426 640 720 854].fzf.first
-
+size=[2, 3, 4, 6, 8].fzf.first.to_i rescue 4
+# size=4 unless size
 scale=640 #unless scale
 
 cmd=[]
 cmd<<'ffmpeg'
 
-cuts.take(4).each_with_index do |(ss, to), i|
+cuts.take(size*size).each_with_index do |(ss, to), i|
   cmd << "-ss #{ss} -to #{to} -i '#{inf}'"
 end
 
-# filter=<<~FFMPEG
-  # [0:a][1:a][2:a][3:a]amix=inputs=4:duration=shortest[a]
-	# [0:v]scale=#{scale}:-1[v0]
-	# [1:v]scale=#{scale}:-1[v1]
-	# [2:v]scale=#{scale}:-1[v2]
-	# [3:v]scale=#{scale}:-1[v3]
-	# [v0][v1]hstack=inputs=2:shortest=1[top]
-  # [v2][v3]hstack=inputs=2:shortest=1[bot]
-  # [top][bot]vstack=inputs=2:shortest=1,scale=1280:720[v]
-# FFMPEG
-filter=<<~FFMPEG
-	[0:v]setpts=PTS-STARTPTS,scale=640:-1[v0]
-	[1:v]setpts=PTS-STARTPTS,scale=640:-1[v1]
-	[2:v]setpts=PTS-STARTPTS,scale=640:-1[v2]
-	[3:v]setpts=PTS-STARTPTS,scale=640:-1[v3]
-	[v0][v1][v2][v3]xstack=inputs=4:shortest=1:layout=0_0|w0_0|0_h0|w0_h0,scale=1280:720[v]
-	[0:a][1:a][2:a][3:a]amix=inputs=4:duration=shortest[a]
-FFMPEG
+filter=IO.popen("ffmatrix.rb #{size} #{size}", &:read)
 
-filter.gsub!(/\n/,";")
+# filter=<<~FFMPEG
+	# [0:v]setpts=PTS-STARTPTS,scale=640:-1[v0]
+	# [1:v]setpts=PTS-STARTPTS,scale=640:-1[v1]
+	# [2:v]setpts=PTS-STARTPTS,scale=640:-1[v2]
+	# [3:v]setpts=PTS-STARTPTS,scale=640:-1[v3]
+	# [v0][v1][v2][v3]xstack=inputs=4:shortest=1:layout=0_0|w0_0|0_h0|w0_h0,scale=1280:720[v]
+	# [0:a][1:a][2:a][3:a]amix=inputs=4:duration=shortest[a]
+# FFMPEG
+
+filter.gsub!(/\n/,'')
 filter.gsub!(/;$/, '')
 
 cmd<< %(-filter_complex '#{filter.chomp}') 
 cmd<< %(-map "[v]") 
 cmd<< %(-map "[a]") 
-# cmd<< '-ac 2'
+cmd<< '-ac 2'
 # cmd<< '-shortest'
-cmd<< "vstacked_#{sane_name}"
+cmd<< "vstacked_#{size}_#{sane_name}"
 
 puts cmd
 
