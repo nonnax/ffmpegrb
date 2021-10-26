9e59eff150348794aa4f6620c81d51c2829403c0
nonnax <inonnax@gmail.com>
2021-10-26 16:28:50 +0800

    fixes

--- ffcut_safe.rb
+++ ffcut_safe.rb
@@ -20,8 +20,13 @@ cuts = cuts_df.map # shared
 cuts.each_with_index do |(ss, to), i|
 	cmd = []
 	cmd << 'ffmpeg'
-  cmd << "-ss #{ss} -i '#{inf}' -to #{to}"
-	cmd << '-c copy'
+  cmd << "-i '#{inf}' -ss #{ss} -to #{to}"
+	# cmd << "-filter:v fps=25,scale=#{size}"
+	# cmd<< '-vf mp=eq2=1:1.68:0.3:1.25:1:0.96:1'
+	cmd << '-c:a copy'
+	cmd << '-c:v libx264'
+	cmd << '-crf 20 -preset slow'
+	cmd << '-pix_fmt yuv420p'
 	cmd << '-movflags +faststart'
 	cmd<< "'vseg_%s_%s.mkv'" % [ss.tr(':', '_'), sane_name]
 	p cmd_str = cmd * ' '
