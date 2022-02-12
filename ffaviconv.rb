0f767decba01290825120ec6fbe3b6f7ed17b18b
nonnax <ironald@gmail.com>
2022-02-12 11:57:45 +0800

    update: ffaviconv.rb

--- ffaviconv.rb
+++ ffaviconv.rb
@@ -13,13 +13,15 @@
 # ffmpeg -i input.mkv -sn -c:a libmp3lame -ar 48000 -ab 128k -ac 2 -c:v libxvid \
     # -crf 24 -vtag DIVX -vf scale=640:480 -aspect 4:3 -mbd rd -flags +mv4+aic \
     # -trellis 2 -cmp 2 -subcmp 2 -g 30 -vb 1500k output.avi
+require 'fflib'
+require 'rubytools/fzf'
 
-infile=ARGV.first
+infile, scale=ARGV
 
 fail "missing input video" unless infile
 
 inf=File.basename(infile, '.*')
-size='640:480'
+p size=RESOLUTIONS.select{|r| r.match(/#{scale}/) }.first || '640:480'
 scale_name='640:480'.split(':').last
 
 cmd=<<~CMD
